83c8e3cd7a064199462abd5ba456faa652698695
robertodecampos <roberto.rfc@live.com>
2019-04-17 02:26:48 -0300

    Adicionando classe de modelo para os logs de tentativas de login

---
+++ API/Models/LogTentativaLogin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    public class LogTentativaLogin : IDisposable
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public DateTime DataTentativa { get; set; }
+        public bool LoginEfetuado { get; set; }
+
+        public void Dispose() { }
+    }
+}
