8043025b7048bc2213477c97410ee700fad909f3
robertodecampos <roberto.rfc@live.com>
2019-04-17 02:31:05 -0300

    Adicionando o Controller para gerenciamento das tentativas de login

---
+++ API/Controllers/TentativaLoginController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using API.DAO;
+using API.Utils;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]/")]
+    [ApiController]
+    [Authorize]
+    public class TentativaLoginController : Controller
+    {
+        private Conexao conexao;
+
+        public TentativaLoginController(Conexao conexao) => this.conexao = conexao;
+
+        [HttpGet]
+        [Route("listar")]
+        public IActionResult Index()
+        {
+            try
+            {
+                string login = User.Claims.Where(c => c.Type == "login").ToArray()[0].Value;
+
+                using (var logTentativaLoginDAO = new LogTentativaLoginDAO(conexao))
+                {
+                    return Ok(logTentativaLoginDAO.ListarPeloLogin(login, null));
+                }
+            } catch (Exception erro)
+            {
+#if DEBUG
+                return StatusCode(500, erro);
+#else
+                return StatusCode(500);
+#endif
+            }
+        }
+    }
+}
